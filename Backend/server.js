80653eff0e3fe210ed6ad3b10a5ebf6647c61a0f
Swati <swasingh175@gmail.com>
2023-07-06 19:25:02 +0530

    initiaizing frontend and backend folders

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const notes = require("./data/notes");
+const env = require('dotenv');
+
+const app = express();
+env.config();
+
+app.get('/', (req,res) => {
+    res.send("API is running..");
+});
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const note = notes.find((n)=> n._id === req.params.id);
+    res.send(note);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
